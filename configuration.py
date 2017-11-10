c7c0776aaf8f508511c20ad039a2c32629c0eef3
manika15 <manika.kapoor90@gmail.com>
2017-11-09 21:46:35 -0800

    database schema MongoDB and RDS

---
+++ configuration.py
@@ -0,0 +1,7 @@
+
+#Postgres AWS RDS
+dsn_database = "socialCommunity"
+dsn_hostname =  "social-community.cznwlohjgx0g.us-west-2.rds.amazonaws.com"
+dsn_port = "5432"
+dsn_uid = "myawsuser"
+dsn_pwd = "myawsuser"      
