da27a916f9bd6af0451dee8d7b4088f0b81b07d6
manika15 <manika.kapoor90@gmail.com>
2017-11-16 15:09:19 -0800

    add drop table

--- createdb.py
+++ createdb.py
@@ -1,6 +1,7 @@
 from index import db
-from models import User, Community
+from models import User, Community, UserModerator, UserCommunity
 
+# db.drop_all()
 db.create_all()
 
 print("DB created.")
