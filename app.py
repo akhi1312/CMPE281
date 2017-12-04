abdccc05aa796f5052bac9d585f2e8673bcf3a12
manika15 <manika.kapoor90@gmail.com>
2017-12-04 01:35:42 -0800

    change port to 80

--- app.py
+++ app.py
@@ -1221,4 +1221,4 @@ print ("Done")
 #     db.session.commit()
 
 if __name__ == '__main__':
-    app.run(debug = True,threaded=True, host = "0.0.0.0")
+    app.run(debug = True,threaded=True, host = "0.0.0.0", port = 80)
